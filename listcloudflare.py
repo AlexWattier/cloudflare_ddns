c244ca8b527463b1fdc128dbb5118ad14f8b0cc8
AlexWattier <78381996+AlexWattier@users.noreply.github.com>
2023-11-28 23:30:59 +0100

    Update listcloudflare.py

--- listcloudflare.py
+++ listcloudflare.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 with open('/usr/src/app/config.json') as config_file:
     config = json.load(config_file)
@@ -6,12 +7,12 @@ with open('/usr/src/app/config.json') as config_file:
 zone_id = config["zone_id"]
 cloudflare_email = config["cloudflare_email"]
 cloudflare_auth_key = config["cloudflare_Auth_Key"]
-url = "https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
+url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
 
 headers = {
     "Content-Type": "application/json",
-    "X-Auth-Email": "{cloudflare_email}",
-    "X-Auth-Key": "{cloudflare_auth_key}"
+    "X-Auth-Email": f"{cloudflare_email}",
+    "X-Auth-Key": f"{cloudflare_auth_key}"
 }
 
 response = requests.request("GET", url, headers=headers)
